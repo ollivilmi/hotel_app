f3d2adff494db23cd0757fe881ab6f7e7b75a45a
ollivilmi <olli.vilmi@gmail.com>
2018-03-04 03:42:44 +0200

    Merge fix (profilepage js)

--- web/assets/scripts/profilescript.js
+++ web/assets/scripts/profilescript.js
@@ -6,25 +6,57 @@
 
 
 
-let editButton;
-let saveButton;
-let cancelButton;
-let formInputFields;
-let requestPicture;
-let profilePopup;
+var editButton;
+var saveButton;
+var cancelButton;
+var formInputFields;
+var requestPicture;
+var profilePopup;
 
-const green = '#4CAF50';
-const red = '#f44336';
-const blue = '#008CBA';
+var green = '#4CAF50';
+var red = '#f44336';
+var blue = '#008CBA';
 
 window.onload = function () {
 
     editButton = document.getElementById('edit-btn');
     // editButton.addEventListener('click', editInfo);
 
+
+    saveButton = document.getElementById('save-btn');
+
+    cancelButton = document.getElementById('cancel-btn');
+
+    formInputFields = document.getElementsByClassName('profile-input');
+
+    console.log(profilePopup);
+    
+    let user = getCookie("user");
+    console.log(user);
+    
+    fetch("/management/r/users/u", {
+        headers: {
+            'user': user
+        }
+    })
+            .then(response => response.json())
+            .then(function(json) {
+                fillProfile(json);
+            })
+            .catch(error => console.log(error));
+            
+    fetch("/management/r/users/manager/getNewUsers", {
+        headers: {
+            'user': user
+        }
+    })
+            .then(response => response.json())
+            .then(function(json) {
+                newUsers(json);
+    })
+            .catch(error => console.log(error));
+    
     requestPicture = document.getElementById('request-profile-picture');
-
-
 
     requestPicture.onmouseover = function () {
         console.log('mouse enter called');
@@ -38,16 +70,6 @@ window.onload = function () {
             profilePopup.style.display = "none";
         }
     };
-
-
-    saveButton = document.getElementById('save-btn');
-
-    cancelButton = document.getElementById('cancel-btn');
-
-    formInputFields = document.getElementsByClassName('profile-input');
-
-    console.log(profilePopup);
-
 };
 
 // Edit user information
@@ -67,7 +89,7 @@ EnableButtons = function () {
 };
 
 EnableInputs = function () {
-    for (let i = 0; i < formInputFields.length; i++) {
+    for (var i = 0; i < formInputFields.length; i++) {
         formInputFields[i].disabled = false;
     }
 };
@@ -78,4 +100,56 @@ ShowInfoCard = function () {
     console.log('show infocard called');
     profilePopup = document.getElementById("info-card-popup");
     profilePopup.classList.toggle('show');
+};
+
+const getCookie = (cname) => {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+const fillProfile = (info) => {
+  let department = document.querySelector("#department");
+  let job = document.querySelector("#job");
+  let firstname = document.querySelector("#firstname");
+  let lastname = document.querySelector("#lastname");
+  let username = document.querySelector("#username");
+  let telnumber = document.querySelector("#telnumber");
+  let email = document.querySelector("#email");
+  
+  department.innerHTML = info[0].department;
+  job.innerHTML = info[0].job;
+  firstname.value = info[0].first_name;
+  lastname.value = info[0].last_name;
+  username.value = info[0].username;
+  telnumber.value = info[0].phone;
+  email.value = info[0].email;  
+};
+
+const newUsers = (info) => {
+    let requestString = "";
+    for (let item of info)
+    {
+        console.log(item);
+        requestString += "<div class='request-person'"
+        +"div class='request-person-static'>"
+        +"<p class='request-container-element' id='request-name'>" + item.firstName + " " + item.lastName + "</p>"
+        +"<img class='request-profile-picture' id='request-profile-picture' src='images/person-icon.png'>"
+        +"<button type='submit' id='accept-request-btn' class='request-container-element request-button' name='request-accept'>Accept</button>"
+        +'<button type="submit" id="decline-request-btn" class="request-container-element request-button" name="request-decline">Decline</button>'
+        +"</div>"
+        +'<span class="popup-text" id="info-card-popup">This will be a popup for more info on the user</span>'
+        +"</div>";
+    }
+    document.querySelector(".request-container").innerHTML = requestString;
 };
