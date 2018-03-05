0bc69f2093a5fede75aafb55ae0e30de1909f0c7
Pyrt <Pyrt@DESKTOP-D2OGGA7.home>
2018-03-05 12:45:18 +0200

    Profile page Edit user info modal ready

--- web/secure/scripts/profilescript.js
+++ web/secure/scripts/profilescript.js
@@ -9,8 +9,16 @@ var user;
 var green = '#4CAF50';
 var red = '#f44336';
 var blue = '#008CBA';
+//variables for modal container
+var modal;
+var registerContainer;
+var closeButton;
 
 window.onload = function () {
+    //Get elements for modal container
+    modal = document.getElementById('modal-edit-id');
+    registerContainer = document.getElementById('register-container');
+    closeButton = document.getElementById('closeButton');
 
     editButton = document.getElementById('edit-btn');
     // editButton.addEventListener('click', editInfo);
@@ -175,4 +183,26 @@ const newUsers = (info) => {
         +"</form>";
     }
     document.querySelector(".request-container").innerHTML = requestString;
+};
+
+//method for opening modal from management edit button
+
+openModal = function() {
+    modal.style.display = 'flex';
+    registerContainer.style.display = 'none';
+};
+
+//methdod for closing the modal if user clicks anywhere else or the cross
+
+closeModal = function() {
+    modal.style.display = 'none';
+    registerContainer.style.display = 'flex';
+};
+
+
+window.onclick = function(event) {
+    if(event.target === modal) {
+        modal.style.display = 'none';
+        registerContainer.style.display = 'flex';
+    };
 };
